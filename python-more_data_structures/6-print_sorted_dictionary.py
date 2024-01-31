82b2530d07b05ea01330fb1ddf97335667cb3619
Fpierr <fanuelp9@gmail.com>
2024-01-31 20:57:08 +0100

    prints a dictionary by ordered keys

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    sorted_key = sorted(a_dictionary.key())
+    for key in sorted_key:
+        print("{}: {}".format(key, a_dictionary[key]))
