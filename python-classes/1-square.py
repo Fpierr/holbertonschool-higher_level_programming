078fd08f7cda124592748a1379607cf24b6320bf
Fpierr <fanuelp9@gmail.com>
2024-02-07 06:52:38 +0100

    initialize a Private instance attribute: size in the class Square

---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """Square class with a private size attribute"""
+
+    def __init__(self, size):
+        """Initialize a new Square instance with a given size."""
+        self.__size = size
