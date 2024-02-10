abad9dfd6a775f47a06b40b02096fc4390fa24b3
Fpierr <fanuelp9@gmail.com>
2024-02-10 18:18:07 +0100

    print texte

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -15,8 +15,7 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    text = text.replace('.', '.\n\n')
-    text = text.replace('?', '?\n\n')
-    text = text.replace(':', ':\n\n')
+    for char in ['.', '?', ':']:
+        text = text.replace(char, f'{char}\n\n')
 
     print(text, end="")
