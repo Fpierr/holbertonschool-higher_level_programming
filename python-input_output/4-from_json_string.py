bfda9ddccf970e1a1d7473f9233735a10172da62
Fpierr <fanuelp9@gmail.com>
2024-02-15 16:10:30 +0100

    returns an object (Python data structure) represented by a JSON string:

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""function that returns the JSON representation of an object (string)"""
 
 
 import json
 
 
 def from_json_string(my_str):
-    """Return an object (Python data structure) represented by a JSON string"""
+    """returns the JSON representation of an object (string)"""
     return json.loads(my_str)
