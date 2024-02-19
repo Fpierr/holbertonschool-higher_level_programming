9f4ce774b8b878a44883b8137d66bf7bb3e7e0b8
Fpierr <fanuelp9@gmail.com>
2024-02-19 15:14:00 +0100

    Write the first class Base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write the first class Base"""
+
+
+class Base:
+    """the base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """define init method"""
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
