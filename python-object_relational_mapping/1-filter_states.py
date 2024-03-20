7a14180bad2cd34b30efd3598d75188c7d04921a
Fpierr <fanuelp9@gmail.com>
2024-03-20 12:37:38 +0100

    lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N (upper N) from the database"""
+"""lists all states with a name starting with N (upper N) from the database"""
 
 import MySQLdb
 import sys
