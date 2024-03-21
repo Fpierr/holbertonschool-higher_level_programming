131fe502eb5a2178bead2172d9564ccfcb9f14c6
Fpierr <fanuelp9@gmail.com>
2024-03-21 11:28:38 +0100

    Displays all values in the states table where name matches the argument

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -26,11 +26,11 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # Prepare SQL query to select states where name matches the argument
-    sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    sql_query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
 
     try:
         # Execute the SQL command with the user input as parameter
-        cursor.execute(sql_query, (state_name,))
+        cursor.execute(sql_query, (state_name + "%",))
 
         # Fetch all the rows in a list of tuples
         results = cursor.fetchall()
