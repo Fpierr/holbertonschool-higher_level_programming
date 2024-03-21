c2d35d1c0bc0356e5d810141d2528a117860619c
Fpierr <fanuelp9@gmail.com>
2024-03-21 20:50:02 +0100

    ists all cities of a specific state from the database hbtn_0e_4_usa

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -27,10 +27,10 @@ if __name__ == "__main__":
 
     try:
         # Prepare SQL query to select cities of the given state
-        sql_query = """SELECT cities.id, cities.name
-                       FROM cities JOIN states ON cities.state_id = states.id
-                       WHERE states.name LIKE BINARY %(state_name)s
-                       ORDER BY cities.id ASC"""
+        sql_query = "SELECT cities.id, cities.name\
+                FROM cities JOIN states ON cities.state_id = states.id\
+                WHERE states.name LIKE BINARY %(state_name)s\
+                ORDER BY cities.id ASC"
 
         # Execute the SQL command
         cursor.execute(sql_query, {'state_name': state_name})
@@ -40,11 +40,4 @@ if __name__ == "__main__":
 
         # Print the result
         for row in results:
-            print(row[1])
-
-    except Exception as e:
-        print("Error:", e)
-
-    finally:
-        # Close the database connection
-        db.close()
+            print(", ".join(row[1])
